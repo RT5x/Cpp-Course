08efe32cc846a419f060d9249e3a45c888abf9bf
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-01 10:01:26 -0700

    Create file2.cpp

---
+++ Functions/Intro to Funtions/file2.cpp
@@ -0,0 +1,8 @@
+#include "person.h"
+
+int Person::person_count = 8;
+Person::Person(const std::string& names_param, int age_param)
+    : full_name{names_param}, age{age_param}{
+        ++person_count;
+
+}
