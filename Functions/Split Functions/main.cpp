4e6585a36a27c351eac899f066a6aed938385848
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-03 08:28:01 -0700

    Update main.cpp

--- Functions/Split Functions/main.cpp
+++ Functions/Split Functions/main.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+#include "compare.h"    // Includes declaration for max() from compare.h (preprocessor)
+#include "compare.cpp"
+
+// Revisiting compilation model across multiple files
+
+
+
+int main(int argc, char ** argv){   
+    int max1 = max(34, 53);
+    std::cout << "max: " << max1 << std::endl;
+
+    int minimum = min(34, 53);
+    std::cout << "min: " << minimum << std::endl;
+
+    int mult = incr_mult(34, 53);
+    std::cout << "incr_mult result : " << mult << "\n";
+
+return 0;
+
+}
 
