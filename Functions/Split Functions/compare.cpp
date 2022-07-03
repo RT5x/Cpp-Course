a154c07dd61abcedf0f2a41790ac5d7a4e9f80fd
A_Ghatta <67446944+RT5x@users.noreply.github.com>
2022-07-03 08:28:28 -0700

    Update compare.cpp

--- Functions/Split Functions/compare.cpp
+++ Functions/Split Functions/compare.cpp
@@ -1,1 +1,16 @@
-
+int max(int a, int b){
+    if (a>b)
+        return a;
+    else    
+        return b;
+}
+    // Defines functions here
+int min(int a, int b){
+    if (a < b)
+        return a;
+    else    
+        return b;
+}
+int incr_mult(int a, int b){
+    return ((++a) * (++b));
+}
